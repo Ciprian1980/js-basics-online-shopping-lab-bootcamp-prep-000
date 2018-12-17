4f23ff9664671f2f43e2869d4a28498dcce49f5f
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-12-17 03:31:57 +0000

    Done.

--- index.js
+++ index.js
@@ -10,30 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 101);
+  var price = Math.floor(Math.random() * 100);
   var object = {itemName: item, itemPrice: price};
   cart.push(object);
   return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  var itemPrice;
   if (cart.length === 0){
     return (`Your shopping cart is empty.`);
   }
   else if(getCart().length === 1){
-    return `In your cart, you have ${cart[0]} at ${itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }
   
-  for (let i = 0; i < cart.length; i++){
     if(cart.length === 2){
-      return`In your cart, you have ${cart.length()['']} at ${cart.itemPrice()['']}, and ${cart.length()['']} at ${cart.itemPrice()}.`;
+      return`In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`+ ', ' +`and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
     }
     else if(cart.length >=3){
-      return`In your cart, you have ${cart.length()} at ${cart.itemPrice()}, and ${cart.length()} at ${cart.itemPrice()}, and ${cart.length()} at ${cart.itemPrice()}}.`;
-    }
-  }
-}
+      var items = [];
+      for (let i = 0; i < cart.length; i++){
+      items.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+     }
+     return `In your cart, you have ${items}`
+   }
+ }
+
 
 
 function total() {
