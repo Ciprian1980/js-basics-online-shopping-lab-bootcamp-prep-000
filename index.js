4a24decc173032b98629e0e6c275885b67ba674b
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-05-21 22:15:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var cartDescription = '';
+  var cartDescription = 'In your cart, you have';
   if (cart.length === 0){
     return (`Your shopping cart is empty.`);
   }
